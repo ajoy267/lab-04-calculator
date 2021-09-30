904595a99ebef681063784c62e6d1ffe30f73809
ajoy267 <andrew.joy12@hotmail.com>
2021-09-30 15:07:19 -0700

    added subtraction html, css, and test functions that were similar to addition

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import {add_two, add} from '../calculations.js';
+import {add_two, add, subtract} from '../calculations.js';
 // import { example } from '../example.js';
 
 const test = QUnit.test;
@@ -18,7 +18,7 @@ test('test add_two ', (expect) => {
     expect.equal(actual, expected);
 });
 
-
+//Adding Calculator Test
 test('add function', (expect) => {
     //Arrange
     const input1 = 3;
@@ -43,4 +43,44 @@ test('add function', (expect) => {
 
     //Assert
     expect.equal(actual, expected, 'Should add 4 + 5');
+});
+
+//Subtraction Calculator Test
+test('Sub function', (expect) => {
+    //Arrange
+    const input1 = 3;
+    const input2 = 2;
+    const expected = 1;
+
+    //Act
+    const actual = subtract(input1, input2);
+
+    //Assert
+    expect.equal(actual, expected, 'Should subtract 3 - 2');
+});
+
+test('Sub function', (expect) => {
+    //Arrange
+    const input1 = 4;
+    const input2 = 1;
+    const expected = 3;
+
+    //Act
+    const actual = subtract(input1, input2);
+
+    //Assert
+    expect.equal(actual, expected, 'Should subtract 4 - 1');
+});
+
+test('Sub function', (expect) => {
+    //Arrange
+    const input1 = 15;
+    const input2 = 9;
+    const expected = 6;
+
+    //Act
+    const actual = subtract(input1, input2);
+
+    //Assert
+    expect.equal(actual, expected, 'Should subtract 15 - 9');
 });
