944814c2bbc3835b2fb1532585213422cec23e85
ajoy267 <andrew.joy12@hotmail.com>
2021-09-30 13:21:55 -0700

    playing around in example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,17 @@
 // IMPORT MODULES under test here:
+import {add_two} from '../calculations.js';
 // import { example } from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test add_two ', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add_two(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
