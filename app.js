13012f83f4d725a6afac480b870708cb13ceed76
ajoy267 <andrew.joy12@hotmail.com>
2021-09-30 15:15:35 -0700

    added subtraction button function to app.js to work on click

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-import {add} from './calculations.js';
+import {add, subtract} from './calculations.js';
 
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-btn');
 const addAnswer = document.getElementById('add-answer');
-//console.log(addInput1, addInput2, addButton, addAnswer);
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-btn');
+const subAnswer = document.getElementById('sub-answer');
 
 addButton.addEventListener('click', () => {
     const value1 = Number(addInput1.value);
     const value2 = Number(addInput2.value);
     const result = add(value1, value2);
-    //console.log(result);
     addAnswer.textContent = result;
+});
+
+subButton.addEventListener('click', () => {
+    const value3 = Number(subInput1.value);
+    const value4 = Number(subInput2.value);
+    const subResult = subtract(value3, value4);
+    subAnswer.textContent = subResult;
 });
