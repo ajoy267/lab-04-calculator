a4fd6914df88be24aa9d700f099bb3cfe82edd16
ajoy267 <andrew.joy12@hotmail.com>
2021-09-30 14:10:49 -0700

    added button click event listener

---
+++ app.js
@@ -0,0 +1,9 @@
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-btn');
+const addAnswer = document.getElementById('add-answer');
+//console.log(addInput1, addInput2, addButton, addAnswer);
+
+addButton.addEventListener('click', () => {
+    console.log('add button is being clicked');
+});
